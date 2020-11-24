7fa75be077642e8001fa5bec32296d85e95ace5a
Thang Vo <thang.vohuynhduy@hungthinhcorp.com.vn>
2020-11-24 10:05:05 +0700

    init update project sso android client app

---
+++ android-client-app-sample2/app/src/main/java/com/wso2is/androidsample2/activities/LoginActivity.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.wso2is.androidsample2.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.wso2is.androidsample2.mgt.AuthStateManager;
+import com.wso2is.androidsample2.mgt.ConfigManager;
+import com.wso2is.androidsample2.R;
+import com.wso2is.androidsample2.oidc.AuthRequest;
+
+import net.openid.appauth.AuthState;
+
+/**
+ * This activity will handle the login view of the application.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        AuthStateManager authStateManager = AuthStateManager.getInstance(this);
+        ConfigManager configuration = ConfigManager.getInstance(this);
+
+        // If the user is authorized, the UserActivity view will be launched.
+        if (authStateManager.getCurrentState().isAuthorized() && !configuration.hasConfigurationChanged()) {
+            Log.i(TAG, "User is already authorized, proceeding to user activity.");
+            startActivity(new Intent(this, UserActivity.class));
+            finish();
+        } else {
+            // If the user is not authorized, the LoginActivity view will be launched.
+            setContentView(R.layout.activity_login);
+            getSupportActionBar().setTitle("");
+
+            // Checks if the configuration is valid.
+            if (!configuration.isValid()) {
+                Log.e(TAG, "Configuration is not valid: " + configuration.getConfigurationError());
+                Toast.makeText(this, "Configuration is not valid!", Toast.LENGTH_SHORT).show();
+                finish();
+            } else {
+                // Discards any existing authorization state due to the change of configuration.
+                if (configuration.hasConfigurationChanged()) {
+                    Log.i(TAG, "Configuration change detected, discarding the old state.");
+                    authStateManager.replaceState(new AuthState());
+                    // Stores the current configuration as the last known valid configuration.
+                    configuration.acceptConfiguration();
+                }
+            }
+            findViewById(R.id.bLogin).setOnClickListener((View view) -> AuthRequest.getInstance(this).doAuth());
+        }
+    }
+}
---
+++ android-client-app-sample2/app/src/main/java/com/wso2is/androidsample2/utils/Constants.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.wso2is.androidsample2.utils;
+
+/**
+ * Stores constants of the application.
+ */
+public final class Constants {
+
+    // Constants related to User Info Request.
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+    public static final String UTF_8 = "UTF-8";
+
+    // Constants related to Configuration.
+    public static final String PREFS_NAME = "config";
+    public static final String KEY_LAST_HASH = "lastHash";
+    public static final String HTTP = "http";
+    public static final String HTTPS = "https";
+
+    // Constants related to AuthState.
+    public static final String STORE_NAME = "AuthState";
+    public static final String KEY_STATE = "state";
+
+    // Constants related to Connection Builder.
+    public static final String PROTOCOL = "TLSv1.2";
+
+    // Constants related to the Application.
+    public static final String APP_NAME = "PICKUP";
+}
